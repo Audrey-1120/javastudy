9b918e796c6f5df833d67c4b9603a1f82bbde32d
Audrey <cw195505@gmail.com>
2024-01-08 16:39:48 +0900

    240108

---
+++ 05_array/src/pkg02_two/MainClass2.java
@@ -0,0 +1,19 @@
+package pkg02_two;
+
+import java.util.Scanner;
+
+public class MainClass2 {
+  
+  
+  public static void method01() {
+   
+    
+  }
+
+  public static void main(String[] args) {
+    method01();
+    
+   
+  }
+
+}
